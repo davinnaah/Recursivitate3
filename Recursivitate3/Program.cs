84dded46c8d3f39c7635fce0a434a8b129d38e85
User <User@DESKTOP-PQKU9A0>
2022-01-13 23:38:46 +0200

    Add project files.

---
+++ Recursivitate3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursivitate3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci.Fibo(0, 1, 1, 8));
+        }
+    }
+}
---
+++ Recursivitate3/fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate3
+{
+    class fibonacci
+    {
+        public static int Fibo(int a, int b, int n, int p)
+        {
+            if (n == p)
+                return a;
+            else
+                return Fibo(b, a + b, n + 1, p);
+        }
+    }
+}
